1b8b0ad75f9abfceb0a09a1700b3d2e70d88d4ea
drzhan0802 <131455417+drzhan0802@users.noreply.github.com>
2023-04-28 20:10:31 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World c++ " << 1 << "m" << " " << 80.5 << "cm" << "\n";
+    
+    printf("hello world c \n");
+    return 0;
+}
