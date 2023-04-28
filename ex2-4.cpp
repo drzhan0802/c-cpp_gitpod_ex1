e89fa4a244f773f901fd11bd386c94f35898ac9c
drzhan0802 <131455417+drzhan0802@users.noreply.github.com>
2023-04-28 20:41:19 +0800

    Update ex2-4.cpp

--- ex2-4.cpp
+++ ex2-4.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 int main(void){
     int height_m = 1, height_cm = 80;
+    float f_height = 1.80;
+    char a = 'b';
     printf(" hello Salena ");
     printf(" my height is %d m %d cm \n",height_m,height_cm);
+    printf(" my height is %f m \n",f_height);
+    printf(" hello %c \n",a);
 }
