a1164bfd3d97049db1a11269ad6c29306f4902a4
drzhan0802 <131455417+drzhan0802@users.noreply.github.com>
2023-05-12 20:53:04 +0800

    Create ex4-2.cpp

---
+++ ex4-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x > 5)
+        printf("x > 5 \n");
+    else if(x <= 3)
+        printf("x <= 3 \n");
+    else
+        printf("x > 3 \n");
+
+    return 0;
+}
