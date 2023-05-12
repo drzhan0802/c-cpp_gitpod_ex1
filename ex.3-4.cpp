423709f2e288ed99e5e103d6f4906b720d838624
drzhan0802 <131455417+drzhan0802@users.noreply.github.com>
2023-05-12 20:41:59 +0800

    Create ex.3-4.cpp

---
+++ ex.3-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int x;
+    x = 4;
+    
+    
+    printf(" x |= 3   ==> %d \n", x |= 3);
+    printf(" x &= 1   ==> %d \n", x &= 1);
+}
