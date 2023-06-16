06f4847795948d8629984c8f32b45a1c9d8daa6c
drzhan0802 <131455417+drzhan0802@users.noreply.github.com>
2023-06-16 20:53:48 +0800

    Create ex8-3.cpp

---
+++ ex8-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //char x[] = "happy";
+    char x[6] = {'h', 'a', 'p', 'p', 'y', '\0'};
+    const int N =9;
+    int a[N] = {0};
+    char b[N] = "hello";
+    int i;
+    for(i=0;x[i] !='\0';i++){
+        printf("%c ",x[i]);
+    }
+    printf("\n");
+    for(i=0;b[i] !='\0';i++){
+        printf("%c \n",b[i]);
+    }
+    cout<<"\nHello World\n";
+    return 0;
+}
